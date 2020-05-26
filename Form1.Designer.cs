bcde97d5287df7088c4d16f87dc24081ed0a1d6b
Andrey Rubanov <MRX7999.rubanoff.and@yandex.by>
2020-05-26 17:03:06 +0300

    Worker Must Work (;

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace BASE_DATA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(23, 32);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(580, 372);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(723, 521);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BASE_DATA.Project_organization_1DataSet project_organization_1DataSet;
+        private System.Windows.Forms.BindingSource categoryBindingSource;
+        private BASE_DATA.Project_organization_1DataSetTableAdapters.CategoryTableAdapter categoryTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDCategoryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qualificationNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameOfTheSecondQualificationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; // подключили 
+
+namespace BASE_DATA
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        
+        public string CmdText = "SELECT * FROM [Category]";
+        
+        public string ConnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\ruban\\source\\repos\\WindowsFormsApp2\\ProjectOrganization.mdb";
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(CmdText, ConnString);
+            // создаем объект DataSet
+            DataSet ds = new DataSet();
+            // заполняем таблицу Order  
+            // данными из базы данных
+            dataAdapter.Fill(ds, "[Category]");
+            dataGridView2.DataSource = ds.Tables[0].DefaultView;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "project_organization_1DataSet.Category". При необходимости она может быть перемещена или удалена.
+            this.categoryTableAdapter.Fill(this.project_organization_1DataSet.Category);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
